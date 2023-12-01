a71423409dc65784e34a6229c47a2b61dcf5fa4a
Zachary <zacharyjolliff15@gmail.com>
2023-11-30 19:46:18 -0500

    image upload working. index.php is where you go to upload, upload.php is the logic for that. images.php is where those images are displayed and displayImages.php is what makes it happen

---
+++ displayImages.php
@@ -0,0 +1,18 @@
+<?php
+$imageFolder = 'uploads/';
+$allowedExtensions = array('jpg', 'jpeg', 'png');
+
+// Get all files in the folder
+$files = scandir($imageFolder);
+
+// Filter out non-image files based on allowed extensions
+$imageFiles = array_filter($files, function($file) use ($allowedExtensions) {
+    $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
+    return in_array(strtolower($fileExtension), $allowedExtensions);
+});
+
+// Display images
+foreach ($imageFiles as $imageFile) {
+    echo "<img src='$imageFolder$imageFile' alt='Image'>";
+}
+?>
