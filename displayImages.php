154b3c22004001e071c475f44ab5f69daf423c58
Zachary <zacharyjolliff15@gmail.com>
2023-11-30 21:01:43 -0500

    images only display based on index sample

--- displayImages.php
+++ displayImages.php
@@ -2,8 +2,14 @@
 $imageFolder = 'uploads/';
 $allowedExtensions = array('jpg', 'jpeg', 'png');
 
+// Print the name of the image folder
+echo "Image Folder: $imageFolder<br>";
+
 // Get all files in the folder
 $files = scandir($imageFolder);
+
+// Print the list of files in the folder (for debugging)
+echo "All Files: " . implode(', ', $files) . "<br>";
 
 // Filter out non-image files based on allowed extensions
 $imageFiles = array_filter($files, function($file) use ($allowedExtensions) {
@@ -11,9 +17,16 @@ $imageFiles = array_filter($files, function($file) use ($allowedExtensions) {
     return in_array(strtolower($fileExtension), $allowedExtensions);
 });
 
-// Display images
-foreach ($imageFiles as $imageFile) {
-    echo "<img src='$imageFolder$imageFile' alt='Image'>";
+// Print the list of image files (for debugging)
+echo "Image Files: " . implode(', ', $imageFiles) . "<br>";
+
+// Display only one image if available
+if (!empty($imageFiles)) {
+    // You can change the index to display a different image
+    $selectedImage = $imageFiles[2]; //BC GIT INDEX STARTS AT 2 just ACCEPT IT 
+
+    echo "<img src='$imageFolder$selectedImage' alt='Image'>";
+} else {
+    echo "No images found.";
 }
 ?>
-<!-- this needs to exist inside of where the user reviews will live -->
