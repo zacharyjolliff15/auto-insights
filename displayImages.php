b17a8bc8c11aef843531732f64c83f4d1c8e8be0
Zachary <zacharyjolliff15@gmail.com>
2023-11-30 21:04:31 -0500

    remove testing stuff

--- displayImages.php
+++ displayImages.php
@@ -2,14 +2,11 @@
 $imageFolder = 'uploads/';
 $allowedExtensions = array('jpg', 'jpeg', 'png');
 
-// Print the name of the image folder
-echo "Image Folder: $imageFolder<br>";
-
 // Get all files in the folder
 $files = scandir($imageFolder);
 
 // Print the list of files in the folder (for debugging)
-echo "All Files: " . implode(', ', $files) . "<br>";
+// echo "All Files: " . implode(', ', $files) . "<br>";
 
 // Filter out non-image files based on allowed extensions
 $imageFiles = array_filter($files, function($file) use ($allowedExtensions) {
@@ -18,7 +15,7 @@ $imageFiles = array_filter($files, function($file) use ($allowedExtensions) {
 });
 
 // Print the list of image files (for debugging)
-echo "Image Files: " . implode(', ', $imageFiles) . "<br>";
+// echo "Image Files: " . implode(', ', $imageFiles) . "<br>";
 
 // Display only one image if available
 if (!empty($imageFiles)) {
